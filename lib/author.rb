41a146afc9568a0076c3042f70ccf94a7c648cc2
Maximo Bautista <max.bautista97@gmail.com>
2019-02-02 23:22:23 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Artist 
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+   def posts
+    @posts
+  end
+  
+  def add_post(title)
+    @posts << title
+    title.author = self
+  end
+  
+  def add_post_by_title(title)
+    new_post  = Post.new(title)
+    @posts << new_post
+    new_post.artist = self
+  end
+  
+  def self.post_count
+    Post.all.count
+  end
+end
